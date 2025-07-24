5950fd2d3c3ece95dda658d134b278dcc5d5872d
Samuel <sam3bett@gmail.com>
2025-07-24 11:48:00 +0300

    initial commit

--- src/data/blog.js
+++ src/data/blog.js
@@ -1,31 +1,59 @@
-import logo from "../assets/logo";
+// import logo from "../assets/logo";
+
+// const blogData = {
+//   name: "Underreacted",
+//   image: logo,
+//   about: "A blog about learning React",
+//   posts: [
+//     {
+//       id: 1,
+//       title: "Components 101",
+//       date: "December 15, 2020",
+//       preview: "Setting up the building blocks of your site",
+//       minutes: 5,
+//     },
+//     {
+//       id: 2,
+//       title: "React Data Flow",
+//       date: "December 11, 2020",
+//       preview: "Passing props is never passé",
+//       minutes: 15,
+//     },
+//     {
+//       id: 3,
+//       title: "Function vs Class Components",
+//       preview: "React, meet OOJS.",
+//       minutes: 47,
+//     },
+//   ],
+// };
+
+// export default blogData;
+
 
 const blogData = {
-  name: "Underreacted",
-  image: logo,
-  about: "A blog about learning React",
+  name: "Overreacted",
+  image: "https://via.placeholder.com/215",
+  about: "A personal blog by Dan. I explain with words and code.",
   posts: [
     {
       id: 1,
-      title: "Components 101",
-      date: "December 15, 2020",
-      preview: "Setting up the building blocks of your site",
-      minutes: 5,
+      title: "The WET Codebase",
+      date: "July 13, 2023",
+      preview: "Come waste your time with me."
     },
     {
       id: 2,
-      title: "React Data Flow",
-      date: "December 11, 2020",
-      preview: "Passing props is never passé",
-      minutes: 15,
+      title: "Goodbye, Clean Code",
+      date: "January 11, 2023",
+      preview: "Let clean code guide you. Then let it go."
     },
     {
       id: 3,
-      title: "Function vs Class Components",
-      preview: "React, meet OOJS.",
-      minutes: 47,
-    },
-  ],
+      title: "My Decade in Review",
+      preview: "A personal reflection on 10 years of blogging."
+    }
+  ]
 };
 
 export default blogData;
